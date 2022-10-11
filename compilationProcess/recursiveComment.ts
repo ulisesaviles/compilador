aa7ee1d36e2b6ef42c7442cef647204e888344e4
Ulises <ulises.aviles@cetys.edu.mx>
2022-10-11 16:57:39 -0700

    Added comments

---
+++ compilationProcess/recursiveComment.ts
@@ -0,0 +1,46 @@
+import { isComment, isLetter, isQuote } from "../helpers/helpers";
+import { reservedWords } from "../helpers/reservedWords";
+import { Pointers } from "../types/pointers";
+import { ReservedWord } from "../types/reservedWords";
+import { Token } from "../types/tokens";
+
+export const recursiveComment = (
+  code: string,
+  state: number = 0,
+  pointers: Pointers
+): {
+  token: Token;
+  pointers: Pointers;
+} => {
+  if (state === 0) {
+    // Detects first quote
+    if (isComment(code.charAt(pointers.search))) {
+      return recursiveComment(code, 1, {
+        current: pointers.current,
+        search: pointers.search + 1,
+      });
+    }
+  } else if (state === 1) {
+    // Keeps looking for characters other than quotes
+    if (code.charAt(pointers.search) == "\n") {
+      return recursiveComment(code, 2, {
+        current: pointers.current,
+        search: pointers.search + 1,
+      });
+    }
+
+    return recursiveComment(code, 1, {
+      current: pointers.current,
+      search: pointers.search + 1,
+    });
+  }
+
+  // Maps value to a reserved keyword
+  const value = code.substring(pointers.current, pointers.search);
+  pointers.current = pointers.search;
+  pointers.search = pointers.current;
+  return {
+    token: ["COMMENT", value],
+    pointers,
+  };
+};
