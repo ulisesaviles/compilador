78e7399fb03a3497bb786cb862e6abf00b97e722
Ulises <ulises.aviles@cetys.edu.mx>
2022-11-10 13:21:04 -0800

    Catched errors and new rules

--- syntacticAnalyzer/syntacticAnalyzer.ts
+++ syntacticAnalyzer/syntacticAnalyzer.ts
@@ -1,19 +1,11 @@
-const defaultInput = "Id * Id + Id $";
-
-const rules: { [rule: string]: string[] } = {
-  E: ["T E_q"],
-  E_q: ["epsilon", "+ T E_q"],
-  T: ["F T_q"],
-  T_q: ["epsilon", "* F T_q"],
-  F: ["(E)", "Id"],
-};
+import rules from "./rules";
+const defaultInput = ["VAR", "string", "a", "=", '"VALUE"', ";"];
 
 const getInputMatchesInProduction = (stack: string[], production: string) => {
   let matches = 0;
   let i = 0;
   let elements = production.split(" ");
   while (true) {
-    console.log(`${stack[stack.length - (i + 1)]} === ${elements[i]}`);
     if (
       i < stack.length - 1 &&
       i < elements.length &&
@@ -26,55 +18,60 @@ const getInputMatchesInProduction = (stack: string[], production: string) => {
   }
   return matches;
 };
-const syntacticAnalyzer = async (input = defaultInput) => {
-  let stack = ["$", "E"];
-  let inputAsArr = input.split(" ");
-  let i = 0;
 
-  while (stack) {
-    console.log("Stack: ", JSON.stringify(stack));
-    let top = stack.pop() as string;
-    console.log("Top: " + top);
+const syntacticAnalyzer = (input = defaultInput): boolean => {
+  try {
+    let stack = ["$", "declaration"];
+    // let inputAsArr = input.split(" ");
+    let i = 0;
 
-    if (top === "$" && inputAsArr[i] === "$") return true;
-    else if (top === inputAsArr[i]) i++;
-    else {
-      let productions = rules[top];
-      let matchesPerProduction: { [key: string]: number } = {};
-      if (productions.length === 1)
-        stack.push(...productions[0].split(" ").reverse());
+    while (stack) {
+      console.log("Stack: ", JSON.stringify(stack));
+      let top = stack.pop() as string;
+      console.log("Top: " + top);
+      console.log(stack.length);
+      if (stack.length === 0) return true;
+      else if (top === input[i]) i++;
       else {
-        console.log(`Input actual: ${inputAsArr[i]}`);
-        productions.forEach((production) => {
-          if (production.includes(inputAsArr[i])) {
-            // Check this if
-            let matches = getInputMatchesInProduction(stack, production);
-            matchesPerProduction[production] = matches;
-          }
-        });
-
-        let bestProduction: null | string = null;
-        Object.keys(matchesPerProduction).forEach((production) => {
-          if (
-            !bestProduction ||
-            (matchesPerProduction[production] != undefined &&
-              matchesPerProduction[production] >
-                matchesPerProduction[bestProduction])
-          )
-            bestProduction = production;
-        });
-
-        if (bestProduction) {
-          console.log(`Se eligió: ${bestProduction}`);
-          stack.push(...(bestProduction as string).split(" ").reverse());
-        } else if (productions.includes("epsilon")) continue;
+        let productions = rules[top];
+        let matchesPerProduction: { [key: string]: number } = {};
+        if (productions.length === 1)
+          stack.push(...productions[0].split(" ").reverse());
         else {
-          return false;
+          console.log(`Input actual: ${input[i]}`);
+          productions.forEach((production) => {
+            if (production.includes(input[i])) {
+              // Check this if
+              let matches = getInputMatchesInProduction(stack, production);
+              matchesPerProduction[production] = matches;
+            }
+          });
+
+          let bestProduction: null | string = null;
+          Object.keys(matchesPerProduction).forEach((production) => {
+            if (
+              !bestProduction ||
+              (matchesPerProduction[production] != undefined &&
+                matchesPerProduction[production] >
+                  matchesPerProduction[bestProduction])
+            )
+              bestProduction = production;
+          });
+
+          if (bestProduction) {
+            console.log(`Se eligió: ${bestProduction}`);
+            stack.push(...(bestProduction as string).split(" ").reverse());
+          } else if (productions.includes("epsilon")) continue;
+          else {
+            throw "Syntactical analyzer error.";
+          }
         }
       }
     }
-    console.log("");
+  } catch (e) {
+    throw `Syntactical analyzer error.`;
   }
+  return false;
 };
 
 export default syntacticAnalyzer;
